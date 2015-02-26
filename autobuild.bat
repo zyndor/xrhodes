10553a3a527e97ba5f5371b109820fc1ab03394a
György Straub <gyorgy@nuclearheart.com>
2015-02-26 06:47:18 +0000

    Added new platforms and --clean building to autobuild.bat.

--- autobuild.bat
+++ autobuild.bat
@@ -9,14 +9,28 @@ if "%1"=="" goto :fail
 
 @echo executing builds...
 @echo on
-"%1/%1.mkb" --no-ide --buildenv=VC10X --debug --x86 --make
-"%1/%1.mkb" --no-ide --buildenv=VC10X --release --x86 --make
-"%1/%1.mkb" --no-ide --buildenv=VC10X --debug --arm --compiler=gcc --make
-"%1/%1.mkb" --no-ide --buildenv=VC10X --release --arm --compiler=gcc --make
-"%1/%1.mkb" --no-ide --buildenv=VC11XWP --buildenv-toolset=WP8 --debug --arm --make
-"%1/%1.mkb" --no-ide --buildenv=VC11XWP --buildenv-toolset=WP8 --release --arm --make
-"%1/%1.mkb" --no-ide --buildenv=VC11XWP --buildenv-toolset=WP8 --debug --x86 --make
-"%1/%1.mkb" --no-ide --buildenv=VC11XWP --buildenv-toolset=WP8 --release --x86 --make
+"%1/%1.mkb" --no-ide --buildenv=VC10X --debug --x86 --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC10X --release --x86 --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC10X --debug --arm --compiler=gcc --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC10X --release --arm --compiler=gcc --make --clean
+
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WP8DEV --debug --arm --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WP8DEV --release --arm --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WP8DEV --debug --x86 --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WP8DEV --release --x86 --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WS8DEV --debug --arm --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WS8DEV --release --arm --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WS8DEV --debug --x86 --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WS8DEV --release --x86 --make --clean
+
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WP81DEV --debug --arm --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WP81DEV --release --arm --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WP81DEV --debug --x86 --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WP81DEV --release --x86 --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WS81DEV --debug --arm --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WS81DEV --release --arm --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WS81DEV --debug --x86 --make --clean
+"%1/%1.mkb" --no-ide --buildenv=VC12 --buildenv-toolset=WS81DEV --release --x86 --make --clean
 @echo off
 
 if "%2"=="-s" call :shutdown
