72914b9a222ede3e6cfc00d79c4ceedea38909a7
György Straub <gyorgy@nuclearheart.com>
2015-08-19 06:29:45 +0100

    XR::Hash 64-bit compatibility work. - due to ambiguity issues, seed is now set using SetSeed().

--- xr3/h/XR/Hash.cpp
+++ xr3/h/XR/Hash.cpp
@@ -11,93 +11,99 @@ namespace XR
 #if defined XR_DEBUG && !defined XR_DEBUG_PERFORMANCE
 typedef std::map<uint32, std::string> StringMap;
 
-static StringMap  s_stringLookup;
-
-static void  ClearStringLookup()
-{
-  s_stringLookup.clear();
-}
-#endif  //XR_DEBUG
+static StringMap	s_stringLookup;
+#endif	//XR_DEBUG
 
 //==============================================================================
-uint32  Hash::String(const char* pString, uint32 hash)
+uint32	Hash::s_seed(Hash::kSeed);
+
+//==============================================================================
+void	Hash::SetSeed(uint32 seed)
 {
-  XR_ASSERT(Hash, pString != 0);
-  int c;
-#if defined XR_DEBUG && !defined XR_DEBUG_PERFORMANCE
-  const char* pSaved(pString);
-#endif  //XR_DEBUG
-  while ((c = *pString) != 0)
-  {
-    hash += (hash << 5) + (hash + tolower(c));
-    ++pString;
-  }
-
-#if defined XR_DEBUG && !defined XR_DEBUG_PERFORMANCE
-  StringMap::iterator iFind(s_stringLookup.find(hash));
-  XR_ASSERTMSG(Hash, iFind == s_stringLookup.end() || strcasecmp(pSaved,
-    iFind->second.c_str()) == 0, ("Hash collision - %s vs %s (%d)", pSaved,
-      iFind->second.c_str(), hash));
-  if (iFind == s_stringLookup.end())
-  {
-    s_stringLookup[hash] = pSaved;
-  }
-#endif  //XR_DEBUG
-
-  return hash;
+	s_seed = seed;
 }
 
 //==============================================================================
-uint32  Hash::String(const char* pString, int size, uint32 hash)
+uint32	Hash::String(const char* pString)
 {
-  XR_ASSERT(Hash, pString != 0);
-    
-  int c;
+	XR_ASSERT(Hash, pString != 0);
+	uint32 hash = s_seed;
+	int c;
 #if defined XR_DEBUG && !defined XR_DEBUG_PERFORMANCE
-  const char* pSaved(pString);
-  int savedSize(size);
-#endif  //XR_DEBUG
-  while (size > 0 && (c = *pString) != 0)
-  {
-    hash += (hash << 5) + (hash + tolower(c));
-    ++pString;
-    --size;
-  }
+	const char* pSaved(pString);
+#endif	//XR_DEBUG
+	while ((c = *pString) != 0)
+	{
+		hash += (hash << 5) + (hash + tolower(c));
+		++pString;
+	}
 
 #if defined XR_DEBUG && !defined XR_DEBUG_PERFORMANCE
-  StringMap::iterator iFind(s_stringLookup.find(hash));
-  // look for index of first non matching character being @a size
-  XR_ASSERTMSG(Hash, iFind == s_stringLookup.end() || strncasecmp(pSaved,
-    iFind->second.c_str(), savedSize) == 0, ("Hash collision - %s vs %s (%d)",
-      pSaved, iFind->second.c_str(), hash));
-  if (iFind == s_stringLookup.end())
-  {
-    s_stringLookup[hash].assign(pSaved, savedSize);
-  }
-#endif  //XR_DEBUG
+	StringMap::iterator iFind(s_stringLookup.find(hash));
+	XR_ASSERTMSG(Hash, iFind == s_stringLookup.end() || strcasecmp(pSaved,
+		iFind->second.c_str()) == 0, ("Hash collision - %s vs %s (%d)", pSaved,
+			iFind->second.c_str(), hash));
+	if (iFind == s_stringLookup.end())
+	{
+		s_stringLookup[hash] = pSaved;
+	}
+#endif	//XR_DEBUG
 
-  return hash;
+	return hash;
 }
 
 //==============================================================================
-uint32  Hash::Data(const void* pData, int32 size, uint32 hash)
+uint32	Hash::String(const char* pString, size_t size)
 {
-  XR_ASSERT(Hash, size >= 0);
-  const char* p0(static_cast<const char*>(pData));
-  const char* p1(p0 + size);
-  while (p0 != p1)
-  {
-    hash += (hash << 5) + (hash + *p0);
-    ++p0;
-  }
-  return hash;
+	XR_ASSERT(Hash, pString != 0);
+	uint32 hash = s_seed;
+	int c;
+#if defined XR_DEBUG && !defined XR_DEBUG_PERFORMANCE
+	const char* pSaved(pString);
+	int savedSize(size);
+#endif	//XR_DEBUG
+	while (size > 0 && (c = *pString) != 0)
+	{
+		hash += (hash << 5) + (hash + tolower(c));
+		++pString;
+		--size;
+	}
+
+#if defined XR_DEBUG && !defined XR_DEBUG_PERFORMANCE
+	StringMap::iterator iFind(s_stringLookup.find(hash));
+	// look for index of first non matching character being @a size
+	XR_ASSERTMSG(Hash, iFind == s_stringLookup.end() || strncasecmp(pSaved,
+		iFind->second.c_str(), savedSize) == 0, ("Hash collision - %s vs %s (%d)",
+			pSaved, iFind->second.c_str(), hash));
+	if (iFind == s_stringLookup.end())
+	{
+		s_stringLookup[hash].assign(pSaved, savedSize);
+	}
+#endif	//XR_DEBUG
+
+	return hash;
 }
 
 //==============================================================================
-void  Hash::DebugClearStringLookup()
+uint32	Hash::Data(const void* pData, size_t size)
+{
+	XR_ASSERT(Hash, size >= 0);
+	uint32 hash = s_seed;
+	const char* p0(static_cast<const char*>(pData));
+	const char* p1(p0 + size);
+	while (p0 != p1)
+	{
+		hash += (hash << 5) + (hash + *p0);
+		++p0;
+	}
+	return hash;
+}
+
+//==============================================================================
+void	Hash::DebugClearStringLookup()
 {
 #if defined XR_DEBUG && !defined XR_DEBUG_PERFORMANCE
-  s_stringLookup.clear();
+	s_stringLookup.clear();
 #endif
 }
 
--- xr3/h/XR/Hash.hpp
+++ xr3/h/XR/Hash.hpp
@@ -20,17 +20,22 @@ namespace XR
 //==============================================================================
 class Hash
 {
-  XR_NONOBJECT_DECL(Hash)
+	XR_NONOBJECT_DECL(Hash)
 
 public:
-  // static
-  static const uint32 kSeed = 61681;
+	// static
+	static const uint32 kSeed = 61681;
 
-  static uint32 String(const char* pString, uint32 hash = kSeed);
-  static uint32 String(const char* pString, int32 size, uint32 hash = kSeed);
-  static uint32 Data(const void* pData, int32 size, uint32 hash = kSeed);
+	static void		SetSeed(uint32 seed);
+	
+	static uint32	String(const char* pString);
+	static uint32	String(const char* pString, size_t size);
+	static uint32	Data(const void* pData, size_t size);
 
-  static void   DebugClearStringLookup();
+	static void		DebugClearStringLookup();
+
+private:
+	static uint32	s_seed;
 };
 
 } // XR
