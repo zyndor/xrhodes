8c46f10c07f19800a15779f988f358d940c10eaa
György Straub <gyorgy@nuclearheart.com>
2017-01-20 06:47:29 +0000

    Remove XR::Dependent<>.

--- xr3/h/XR/fundamentals.hpp
+++ xr3/h/XR/fundamentals.hpp
@@ -30,30 +30,6 @@ namespace XR
 enum  { INVALID_ID = -1 };
 
 //==============================================================================
-///@brief The Dependent template allows to navigate a hierarchy of classes
-/// upwards. E.g. to get from a Room to a Level to a World to a Game.
-template  <class Parent>
-class  Dependent
-{
-public:
-  // types
-  typedef Parent  ParentType;
-  
-  // structors
-  Dependent();
-  explicit Dependent(Parent* p);
-  ~Dependent();
-
-  // general use
-  Parent*  GetParent();
-
-  void  SetParent(Parent* p);
-
-private:
-  // data
-  Parent* m_pParent;
-};
-
 ///@brief
 template  <class T>
 class Singleton
@@ -93,40 +69,6 @@ private:
 
 //==============================================================================
 // implementation
-//==============================================================================
-template  <class Parent>
-Dependent<Parent>::Dependent()
-: m_pParent(0)
-{}
-
-//==============================================================================
-template  <class Parent>
-Dependent<Parent>::Dependent(Parent* p)
-: m_pParent(p)
-{}
-
-//==============================================================================
-template  <class Parent>
-Dependent<Parent>::~Dependent()
-{}
-
-//==============================================================================
-///@return  The object's parent.
-template  <class Parent>
-inline
-Parent*  Dependent<Parent>::GetParent()
-{
-  return m_pParent;
-}
-
-//==============================================================================
-///@brief Sets @a p as the object's parent.
-template  <class Parent>
-void  Dependent<Parent>::SetParent(Parent* p)
-{
-  m_pParent = p;
-}
-
 //==============================================================================
 template  <class T>
 T* Singleton<T>::s_pInstance = 0;
