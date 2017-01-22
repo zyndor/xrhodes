5b6dd8b314b4d3de011f7884113547e6905308c4
György Straub <gyorgy@nuclearheart.com>
2017-01-22 23:06:55 +0000

    Rewritten XR::Worker in C++11 with standard threading facilities. - A thread is started and stopped when jobs become available and have completed, respectively, - We maintain a number of attempts and a sleep interval, within which the thread tries to see if jobs are available. - The thread cannot be Abort()ed; Finalize() can be used to join() it and let it finish its processing - it is now mandatory before the destructor is reached. - Removed the notion of stacksize and externally checking whether the Worker IsBusy().

--- xr3/h/XR/SDL/Worker.cpp
+++ xr3/h/XR/SDL/Worker.cpp
@@ -7,75 +7,120 @@
 #include <SDL_thread.h>
 #include <XR/Worker.hpp>
 #include <XR/debug.hpp>
+#include <XR/RefHolder.hpp>
+#include <XR/ScopeGuard.hpp>
 
 namespace XR
 {
 
-int StartRoutine( void* p)
+//==============================================================================
+void Worker::ThreadFunction(Worker& worker)
 {
-  Worker::Core      *pWorker(static_cast<Worker::Core*>(p));
-  const bool&       isQuitting(pWorker->isQuitting);
-  Worker::JobQueue& jobs(pWorker->jobs);
-  SDL_mutex*        pLock(static_cast<SDL_mutex*>(pWorker->pLockImpl));
-
-  while (!isQuitting || !jobs.empty())
-  {
-    SDL_mutexP(pLock);
-    Worker::Job j(jobs.front());
-    jobs.pop_front();
-    SDL_mutexV(pLock);
-
-    (*j.pJobCb)(j.pData);
-  }
-
-  return 0;
+  worker.Loop();
 }
 
-Worker::Worker(int stackSize)
-: m_core()
-{
-  XR_ASSERT(Worker, stackSize > 0);
-  m_core.pStack = new char[stackSize];
-  m_core.pLockImpl = SDL_CreateMutex();
-  m_core.pThreadImpl = SDL_CreateThread(StartRoutine, "XR Worker", this);
-}
+//==============================================================================
+Worker::Worker()
+: m_isThreadActive(false),
+  m_numAttempts(100),
+  m_sleepIntervalMs(10)
+{}
 
+//==============================================================================
 Worker::~Worker()
 {
-  m_core.isQuitting = true;
-
-  SDL_WaitThread(static_cast<SDL_Thread*>(m_core.pThreadImpl), 0);
-  SDL_DestroyMutex(static_cast<SDL_mutex*>(m_core.pLockImpl));
-  delete[] static_cast<char*>(m_core.pStack);
+  XR_ASSERTMSG(Worker, !m_isThreadActive && !m_thread.joinable(),
+    "Jobs must finish processing before you hit the destructor. Try Finalize().");
 }
 
-bool Worker::IsBusy() const
+//==============================================================================
+void Worker::SetIdleThreadExpiry(int numAttempts, int sleepIntervalMs)
 {
-  SDL_mutex*  pLock(static_cast<SDL_mutex*>(m_core.pLockImpl));
-  SDL_mutexP(pLock);
-  bool  result(!m_core.jobs.empty());
-  SDL_mutexV(pLock);
-  return result;
+  m_numAttempts = numAttempts;
+  m_sleepIntervalMs = sleepIntervalMs;
 }
 
-void Worker::Do( JobCallback pJobCb, void* pData )
+//==============================================================================
+void  Worker::Enqueue(Job j)
 {
-  XR_ASSERT(Worker, pJobCb != 0);
-
-  Job j = { pJobCb, pData };
-
-  SDL_mutex*  pLock(static_cast<SDL_mutex*>(m_core.pLockImpl));
-  SDL_mutexP(pLock);
-  m_core.jobs.push_back(j);
-  SDL_mutexV(pLock);
+  if (m_isThreadActive)
+  {
+    std::unique_lock<std::mutex> lock(m_jobsMutex);
+    m_jobs.push_back(j);
+  }
+  else
+  {
+    m_jobs.push_back(j);
+    if (m_thread.joinable())
+    {
+      m_thread.join();
+    }
+    m_thread = std::thread(ThreadFunction, MakeRefHolder(*this));
+  }
 }
 
-void  Worker::Abort()
+//==============================================================================
+void  Worker::Finalize()
 {
-  SDL_mutex*  pLock(static_cast<SDL_mutex*>(m_core.pLockImpl));
-  SDL_mutexP(pLock);
-  m_core.jobs.clear();
-  SDL_mutexV(pLock);
+  if (m_thread.joinable())
+  {
+    m_thread.join();
+  }
+}
+
+//==============================================================================
+void  Worker::Loop()
+{
+  m_isThreadActive = true;
+  auto guard = MakeScopeGuard([this]()
+  {
+    m_isThreadActive = false;
+  });
+
+  const int numAttempts = m_numAttempts;
+  const auto sleepInterval = std::chrono::milliseconds(m_sleepIntervalMs);
+  int attemptsLeft = m_numAttempts;
+  while (true)
+  {
+    // check if we have jobs
+    bool hasJob;
+    {
+      std::unique_lock<std::mutex>  lock(m_jobsMutex);
+      hasJob = !m_jobs.empty();
+    }
+
+    if (hasJob)
+    {
+      // reset number of attemptsLeft
+      attemptsLeft = m_numAttempts;
+
+      // access job
+      auto& j = m_jobs.front();
+      auto jobGuard = MakeScopeGuard([this]()
+      {
+        // remove from queue
+        std::unique_lock<std::mutex>  lock(m_jobsMutex);
+        m_jobs.pop_front();
+      });
+
+      // perform job
+      (*j.pJobCb)(j.pData);
+    }
+    else
+    {
+      // decrease number of attemptsLeft.
+      --attemptsLeft;
+      if (attemptsLeft > 0)
+      {
+        std::this_thread::sleep_for(sleepInterval);
+      }
+      else
+      {
+        // we've been idle for too long, thread's going to finish now.
+        break;
+      }
+    }
+  }
 }
 
 } // XR
--- xr3/h/XR/Worker.hpp
+++ xr3/h/XR/Worker.hpp
@@ -9,12 +9,22 @@
 
 #include "Queue.hpp"
 #include "fundamentals.hpp"
+#include <thread>
+#include <mutex>
+#include <atomic>
 
 namespace XR
 {
 
 //==============================================================================
-///@brief Queues jobs and does them asynchronously.
+///@brief Worker runs a thread to perform Jobs on. The thread is only started
+/// when jobs are enqueued, and once the queue of jobs have finished processing,
+/// the Worker thread will check for new jobs a given number of times with a
+/// given interval between each before it's terminated (Default is 100 times
+/// with 10 milliseconds).
+///@note The user must make sure that all jobs have finished processing before
+/// the execution reaches the Worker destructor. Finalize() helps facilitate
+/// this.
 class Worker
 {
   XR_NONCOPY_DECL(Worker)
@@ -29,35 +39,42 @@ public:
     void*       pData;
   };
 
-  typedef XR::Queue<Job>  JobQueue;
-
-  struct  Core
-  {
-    JobQueue    jobs;
-    bool        isQuitting;
-
-    void*       pStack;
-    void*       pLockImpl;
-    void*       pThreadImpl;
-  };
-
   // structors
-  explicit Worker(int stackSize);
+  explicit Worker();
   ~Worker();
 
   // general
-  ///@return  Whether there are any jobs on the queue.
-  bool  IsBusy() const;
+  ///@brief Sets the number of retries and the interval between them
+  /// for when the thread has no jobs.
+  ///@note Changes will take place after the thread (re-)launch.
+  void  SetIdleThreadExpiry(int numAttempts, int sleepIntervalMs);
 
-  ///@brief Adds a job to the queue.
-  void  Do(JobCallback pJobCb, void* pData); // no ownership transfer
+  ///@brief Adds a job to the queue. If the thread wasn't running, this
+  /// will start it.
+  void  Enqueue(Job job);
 
-  ///@brief  Removes all jobs from the queue.
-  void  Abort();
+  ///@brief Finishes the processing of jobs. Blocking call.
+  void  Finalize();
 
 private:
+  // types
+  typedef XR::Queue<Job>  JobQueue;
+
   // data
-  Core  m_core;
+  mutable std::mutex  m_jobsMutex;
+  JobQueue            m_jobs;
+
+  std::atomic<bool>   m_isThreadActive;
+  std::thread         m_thread;
+
+  int                 m_numAttempts;
+  int                 m_sleepIntervalMs;
+
+  // static
+  static void ThreadFunction(Worker& worker);
+
+  // internal
+  void Loop();
 };
 
 } // XR
