d3d47a03a32cc040984ff5c37f66ef245f90901a
György Straub <gyorgy@nuclearheart.com>
2017-03-08 08:32:37 +0000

    Updated xrgl.hpp with new functionality. - moved SDL includes to the only place they're needed.

--- xr3/h/XR/SDL/xrgl.hpp
+++ xr3/h/XR/SDL/xrgl.hpp
@@ -7,35 +7,60 @@
 #ifndef XR_XRGL_HPP
 #define XR_XRGL_HPP
 
-#include <XR/Log.hpp>
 #include <XR/debug.hpp>
 #include <GL/glew.h>
 #include <SDL_opengl.h>
-#include <SDL_video.h>
 
 namespace XR
+{
+namespace GL
+{
+
+GLint CheckError(char const* pText);
+
+void SwitchEnabledState(GLenum name, bool state);
+
+} // GL
+} // XR
+
+//==============================================================================
+#define XR_GL_CALL(theCall)  theCall; XR::GL::CheckError( #theCall )
+
+//==============================================================================
+#define XR_GL_CALL_RESULT(theCall, result)\
+  theCall;\
+  result = XR::GL::CheckError( #theCall )
+
+//==============================================================================
+// inline
+//==============================================================================
+namespace XR
+{
+namespace GL
 {
 
 //==============================================================================
 inline
-GLint  CheckGLError(const char* pText)
+GLint  CheckError(char const* pText)
 {
-  GLint error(glGetError());
-  if (error != 0)
+  GLint error = glGetError();
+  if (error != GL_NO_ERROR)
   {
-    XR_TRACE(CheckGLError, ("%s: %d", pText, error));
+    XR_ERROR(("OpenGL error %d occurred attempting '%s'.", error, pText));
   }
-  XR_ASSERTMSG(GL, error == GL_NONE, ("OpenGL Error: %d (attempting: %s)",
-    error, pText));
   return error;
 }
 
 //==============================================================================
-#define XR_GL_CALL(theCall)  theCall; CheckGLError( #theCall )
-
-//==============================================================================
-#define XR_GL_CALL_RESULT(theCall, result)  theCall; result = CheckGLError( #theCall )
-
+inline
+void SwitchEnabledState(GLenum name, bool state)
+{
+  auto fn = state ? glEnable : glDisable;
+  XR_GL_CALL(fn(name));
 }
+
+} // GL
+} // XR
+
 
 #endif // XR_XRGL_HPP
