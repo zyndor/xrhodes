696bd5a5319f4d6b8b7dd15905b5722304033f33
György Straub <gyorgy@nuclearheart.com>
2017-07-20 18:13:02 +0100

    Updated / fixed WorkerTests.

--- unittests/src/WorkerTests.cpp
+++ unittests/src/WorkerTests.cpp
@@ -10,51 +10,70 @@ namespace XR
   TEST_CLASS(WorkerTests)
   {
   public:
+    struct Job
+    {
+      int durationMs;
+      int executed = 0;
+      int cancelled = 0;
+      std::mutex mutex; // we're operating on the same instance, might be executing and cancelling at the same time.
+    };
+
     static void JobFn(void* pData)
     {
-      int* pInt = static_cast<int*>(pData);
-      if (pInt)
+      Job* j = static_cast<Job*>(pData);
+      std::unique_lock<decltype(Job::mutex)> lock(j->mutex);
+      if (j->durationMs > 0)
       {
-        if (*pInt > 0)
-        {
-          std::this_thread::sleep_for(std::chrono::milliseconds(*pInt));
-        }
-
-        ++*pInt;
+        std::this_thread::sleep_for(std::chrono::milliseconds(j->durationMs));
       }
+
+      ++j->executed;
     }
 
     TEST_METHOD(Worker_JobDone_Finalize)
     {
       auto worker = std::make_unique<Worker>();
-      worker->SetIdleThreadExpiry(0, 0);
 
-      int i = 100;
-      worker->Enqueue({ JobFn, &i });
+      Job j;
+      j.durationMs = 100;
+      worker->Enqueue({ JobFn, nullptr, &j });
       worker->Finalize();
       worker.reset();
 
-      Assert::IsTrue(i == 101);
+      Assert::IsTrue(j.executed == 1);
+    }
+
+    static void CancelFn(void* pData)
+    {
+      Job* j = static_cast<Job*>(pData);
+      std::unique_lock<decltype(Job::mutex)> lock(j->mutex);
+      ++j->cancelled;
     }
 
     TEST_METHOD(Worker_CancelPendingJobs)
     {
       auto worker = std::make_unique<Worker>();
-      worker->SetIdleThreadExpiry(0, 0);
 
-      int i = 100;
-      const int kNumIterations = 3;
-      for(int j = 0; j < kNumIterations; ++j)
+      Job j;
+      j.durationMs = 100;
+      const int kNumIterations = 5;
+      for(int i = 0; i < kNumIterations; ++i)
       {
-        worker->Enqueue({ JobFn, &i });
+        worker->Enqueue({ JobFn, CancelFn, &j });
       }
+
       worker->CancelPendingJobs();
       auto workerGuard = MakeScopeGuard([&worker]()
       {
         worker->Finalize();
       });
 
-      Assert::IsTrue(i < 100 + kNumIterations);
+      // We don't know how many it will manage to get through, but it can't be all of them.
+      Assert::IsTrue(j.cancelled > 0);
+
+      wchar_t buffer[256];
+      swprintf(buffer, L"%d enqueued, %d executed, %d cancelled does not add up.", kNumIterations, j.executed, j.cancelled);
+      Assert::IsTrue(j.executed + j.cancelled == kNumIterations, buffer);
     }
   };
 }
