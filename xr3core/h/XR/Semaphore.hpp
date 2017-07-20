cc6e1607b099f781063d4528c7adee4ffd2edebf
György Straub <gyorgy@nuclearheart.com>
2017-07-20 08:19:46 +0100

    Break out Semaphore's innards, making it easier to use a waited counter with a pre-existing lock.

--- xr3core/h/XR/Semaphore.hpp
+++ xr3core/h/XR/Semaphore.hpp
@@ -13,44 +13,78 @@
 namespace XR
 {
 
-// Simple semaphore implementation based on threading primitives available in C++11.
+//=============================================================================
+///@brief Simple semaphore implementation based on threading primitives
+/// available in C++11.
 class Semaphore
 {
 public:
+  // types
+  using Mutex = std::mutex;
+  using UniqueLock = std::unique_lock<Mutex>;
+
+  ///@brief Enables client code to synchronize to its pre-defined mutex.
+  class Core
+  {
+  public:
+    void Post()
+    {
+      ++m_counter;
+      m_cv.notify_one();
+    }
+
+    void Wait(UniqueLock& lock)
+    {
+      while (m_counter == 0) // Handle spurious wake-ups.
+      {
+        m_cv.wait(lock);
+      }
+      --m_counter;
+    }
+
+    bool TryWait()
+    {
+      if (m_counter > 0)
+      {
+        --m_counter;
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+  private:
+    // data
+    std::condition_variable m_cv;
+    uint32_t m_counter = 0; // Initialized as locked.
+  };
+
+
   // general
   void Post()
   {
-    std::unique_lock<decltype(m_mutex)> lock(m_mutex);
-    ++m_counter;
-    m_cv.notify_one();
+    UniqueLock lock(m_mutex);
+    m_core.Post();
   }
 
   void Wait()
   {
-    std::unique_lock<decltype(m_mutex)> lock(m_mutex);
-    while (!m_counter) // Handle spurious wake-ups.
-    {
-      m_cv.wait(lock);
-    }
-    --m_counter;
+    UniqueLock lock(m_mutex);
+    m_core.Wait(lock);
   }
 
   bool TryWait()
   {
-    std::unique_lock<decltype(m_mutex)> lock(m_mutex);
-    if (m_counter)
-    {
-      --m_counter;
-      return true;
-    }
-    return false;
+    UniqueLock lock(m_mutex);
+    return m_core.TryWait();
   }
 
 private:
   // data
-  std::mutex m_mutex;
-  std::condition_variable m_cv;
-  uint32_t m_counter = 0; // Initialized as locked.
+  Mutex m_mutex;
+  Core m_core;
 };
 
 }
