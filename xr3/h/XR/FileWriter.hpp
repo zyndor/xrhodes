e473bd0241bde60ac693cd2b7c3705c4ba8ce19f
György Straub <gyorgy@nuclearheart.com>
2017-08-20 06:48:32 +0100

    Added XR::FileWriter.

---
+++ xr3/h/XR/FileWriter.hpp
@@ -0,0 +1,47 @@
+//
+// XRhodes
+//
+// copyright (c) Nuclear Heart Interactive Ltd. All rights reserved.
+//
+//==============================================================================
+#ifndef XR_FILEWRITER_HPP
+#define XR_FILEWRITER_HPP
+
+#include "File.hpp"
+#include <XR/fundamentals.hpp>
+
+namespace XR
+{
+
+//==============================================================================
+///@brief Facilitates writing to a file.
+class FileWriter
+{
+  XR_NONCOPY_DECL(FileWriter)
+
+public:
+  // types
+  enum class Mode
+  {
+    Truncate,
+    Append,
+  };
+
+  // structors
+  ~FileWriter();
+
+  // general
+  bool Open(File::Path const& path, Mode mode, bool text);
+  bool Write(void const* buffer, size_t elemSize, size_t numElems);
+  size_t GetSize() const;
+
+  void Close();
+
+private:
+  // data
+  File::Handle m_handle = nullptr;
+};
+
+}
+
+#endif //XR_FILEWRITER_HPP
---
+++ xr3/src/FileWriter.cpp
@@ -0,0 +1,59 @@
+//
+// XRhodes
+//
+// copyright (c) Nuclear Heart Interactive Ltd. All rights reserved.
+//
+//==============================================================================
+#include <XR/FileWriter.hpp>
+
+namespace XR
+{
+
+//==============================================================================
+FileWriter::~FileWriter()
+{
+  Close();
+}
+
+//==============================================================================
+char const* kMode[] =
+{
+  "wb",
+  "ab",
+  "w",
+  "a"
+};
+
+bool FileWriter::Open(File::Path const& path, Mode mode, bool text)
+{
+  Close();
+
+  auto const offset = (2 * (text ? 1 : 0));
+  auto handle = File::Open(path, kMode[static_cast<int>(mode) + offset]);
+  m_handle = handle;
+  return handle != nullptr;
+}
+
+//==============================================================================
+bool FileWriter::Write(void const* buffer, size_t elemSize, size_t numElems)
+{
+  return File::Write(buffer, elemSize, numElems, m_handle) == numElems;
+}
+
+//==============================================================================
+size_t FileWriter::GetSize() const
+{
+  return m_handle ? File::GetSize(m_handle) : 0;
+}
+
+//==============================================================================
+void FileWriter::Close()
+{
+  if (m_handle)
+  {
+    File::Close(m_handle);
+    m_handle = nullptr;
+  }
+}
+
+} // XR
