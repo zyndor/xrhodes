4a5e557eb769dae6424e0ef63d5e20752491869d
György Straub <gyorgy@nuclearheart.com>
2017-08-20 06:45:08 +0100

    Added / fixed docs.

--- xr3/h/XR/File.hpp
+++ xr3/h/XR/File.hpp
@@ -51,18 +51,24 @@ public:
   static bool   CheckExists(Path const& name);
 
   ///@brief Attempts to open a file in the given @a mode (standard fopen flags
-  /// apply) and return a handle to it. If a file in the given path was not found,
-  /// then attempts will be made to append the ram and rom paths (given to Init())
-  /// to @a name and open the file there, in this order.
+  /// apply) and return a handle to it. If a file in the given path was not 
+  /// found, then attempts will be made to locate the file in the ram and rom
+  /// paths (given to Init()) by prepending @a name with them if possible
+  /// (i.e. within the capacity of Path), in this order.
   static Handle Open(Path const& name, const char* mode);
 
   static size_t GetSize(Handle hFile);
+
+  ///@brief Number of elements actually written.
   static size_t Read(Handle hFile, size_t elemSize, size_t numElems, void* parBuffer);
+
+  ///@brief Number of elements actually written.
   static size_t Write(const void* parBuffer, size_t elemSize, size_t numElems, Handle hFile);
+
   static size_t Tell(Handle hFile);
 
   ///@brief Repositions the position indicator for the given file handle.
-  ///@return Boolean to indicate the success of the operation. Note how this is 
+  ///@return Boolean to indicate the success of the operation. Note how this is
   /// contrary to fseek(), which returns 0 on success.
   static bool   Seek(Handle hFile, size_t offset, SeekFrom sf);
 
