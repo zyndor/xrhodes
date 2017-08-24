e608f71e157d01b80edeef1047b494caa9b791c0
György Straub <gyorgy@nuclearheart.com>
2017-08-24 07:59:10 +0100

    Added XR::Counter<>, XR::Frame<> and a few possible threading policies.

---
+++ xr3core/h/XR/Counter.hpp
@@ -0,0 +1,119 @@
+//
+// XRhodes
+//
+// copyright (c) Nuclear Heart Interactive Ltd. All rights reserved.
+//
+//==============================================================================
+#ifndef XR_COUNTER_HPP
+#define XR_COUNTER_HPP
+
+#include "debug.hpp"
+#include "ScopeGuard.hpp"
+#include "fundamentals.hpp"
+#include <mutex>
+
+namespace XR
+{
+
+//==============================================================================
+struct SingleThreaded
+{
+  void Lock() {}
+  void Unlock() {}
+};
+
+struct Mutexed
+{
+  void Lock()
+  {
+    m_mutex.lock();
+  }
+
+  void Unlock()
+  {
+    m_mutex.unlock();
+  }
+
+private:
+  std::mutex m_mutex;
+};
+
+//==============================================================================
+///@brief Helps to tie the acquisition and release of resources to a scope, in
+/// the vein of std::unique_lock<> and ScopeGuard.
+template <class T, void(T::*Enter)(), void(T::*Exit)()>
+class Frame
+{
+  XR_NONCOPY_DECL(Frame)
+
+public:
+  // structors
+  Frame(T& obj)
+  : m_frameObject(obj)
+  {
+    (obj.*Enter)();
+  }
+
+  ~Frame()
+  {
+    (m_frameObject.*Exit)();
+  }
+
+private:
+  T& m_frameObject;
+};
+
+//==============================================================================
+///@brief Facilitates counting of references, intstances, locking objects,
+/// in your threading environment of choice.
+template <class ThreadingPolicy = SingleThreaded>
+class Counter
+{
+public:
+  using ThreadingScope = Frame<ThreadingPolicy, &ThreadingPolicy::Lock,
+    &ThreadingPolicy::Unlock>;
+
+  // structors
+  ~Counter()
+  {
+    XR_ASSERTMSG(Counter, m_counter == 0, ("Destructor called with %d references leaked."));
+  }
+
+  // general
+  void Acquire()
+  {
+    ThreadingScope lock(m_threading);
+    ++m_counter;
+  }
+
+  void Release(bool& expired)
+  {
+    ThreadingScope lock(m_threading);
+    XR_ASSERTMSG(Counter, m_counter > 0, ("Counter %p underrun.", this));
+    expired = m_counter == 1;
+    --m_counter;
+  }
+
+  void Release()
+  {
+    ThreadingScope lock(m_threading);
+    XR_ASSERTMSG(Counter, m_counter > 0, ("Counter %p underrun.", this));
+    --m_counter;
+  }
+
+  ///@return Whether the counter had been Acquire()d.
+  bool IsEngaged() const
+  {
+    ThreadingScope lock(m_threading);
+    return m_counter > 0;
+  }
+
+private:
+  // data
+  int m_counter = 0;
+  mutable ThreadingPolicy m_threading;
+};
+
+} // XR
+
+#endif //XR_COUNTER_HPP
