bb2184d35b84fe3b3055d72a121f817ac12d6256
György Straub <gyorgy@nuclearheart.com>
2017-08-31 06:47:35 +0100

    Added code docs.

--- xr3core/h/XR/FilePath.hpp
+++ xr3core/h/XR/FilePath.hpp
@@ -15,6 +15,9 @@ namespace XR
 {
 
 //==============================================================================
+///@brief File path abstraction with helpers for frequently used operations and
+/// max supported size built in.
+///@note Converts backslashes to kDirSeparator upon assignment and construction.
 class FilePath: public HardString<XR_MAX_PATH>
 {
 public:
@@ -33,28 +36,39 @@ public:
   }
 
   // general
+  ///@brief Replaces all backslashes with kDirSeparator.
   void NormalizeDirSeparators();
 
+  ///@brief Appends a kDirSeparator to this only if it doesn't already end with one.
   void AppendDirSeparator();
 
+  ///@return The filename and extension part of the path, if any.
   char const* GetNameExt() const;
+
+  ///@return The extension part of the path, which follows the last kExtSeparator
+  /// in the filename, if any.
   char const* GetExt() const;
 
+  ///@return Does this start with the first @a strSize bytes of @a str?
   bool StartsWith(char const* str, size_t strSize) const
   {
     return strncmp(m_arBuffer, str, strSize) == 0;
   }
 
+  ///@return Does this start with the @a str?
   bool StartsWith(char const* str) const
   {
     return StartsWith(str, strlen(str));
   }
 
+  ///@return Does this start with @a other?
   bool StartsWith(FilePath const& other) const
   {
     return StartsWith(other.m_arBuffer);
   }
 
+  ///@brief Removes the last name from the path. If the path ends with a
+  /// kDirSeparator, then it removes that first.
   void Up();
 
   // operators
@@ -65,6 +79,9 @@ public:
     return *this;
   }
 
+  ///@brief Appends a kDirSeparator and the given path (with directory separators
+  /// unified). Prevents duplicate dir separators at the boundary of this and
+  /// the other path only.
   FilePath& operator/=(char const* path)
   {
     AppendDirSeparator();
