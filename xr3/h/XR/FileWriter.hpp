dfebab35b9024039f8d37e298d8d451803a45665
György Straub <gyorgy@nuclearheart.com>
2017-11-04 07:24:25 +0000

    Added convenience functionality to FileWriter for writing out objects directly + tidied up docs.

--- xr3/h/XR/FileWriter.hpp
+++ xr3/h/XR/FileWriter.hpp
@@ -32,10 +32,33 @@ public:
   ~FileWriter();
 
   // general
+  ///@brief Opens a file at @a path for writing in the given @a mode, as
+  /// @a text, optionally (as binary if false).
+  ///@return The success of the operation.
   bool Open(FilePath const& path, Mode mode, bool text);
+
+  ///@brief Attempts to write @a numElems * @a elemSize bytes of @a buffer, to
+  /// the file.
+  ///@returns Whether it's succeeded.
   bool Write(void const* buffer, size_t elemSize, size_t numElems);
+
+  ///@brief Writes an object to the buffer.
+  ///@note Not for aggregate types with pointer members.
+  template <typename T>
+  bool Write(T const& val)
+  {
+    return Write(&val, sizeof(val), 1);
+  }
+
+  ///@brief Safeguard against accidentally trying to write pointers.
+  template <typename T>
+  bool Write(T const* val) = delete;
+
+  ///@return The size of the File being written.
   size_t GetSize() const;
 
+  ///@brief Closes the file. Called automatically when FileWriter goes out of
+  /// scope.
   void Close();
 
 private:
