a019e7b40b9743482ffe0d8440cee923e0388e56
György Straub <gyorgy@nuclearheart.com>
2017-12-27 05:51:11 +0000

    FIX: HashTests now reset the seed once they're done.

--- unittests/src/HashTests.cpp
+++ unittests/src/HashTests.cpp
@@ -7,6 +7,21 @@
 
 namespace XR
 {
+  class HashTests : public ::testing::Test
+  {
+  public:
+    static void SetUpTestCase()
+    {}
+
+    static void TearDownTestCase()
+    {}
+
+    ~HashTests()
+    {
+      Hash::SetSeed(Hash::kSeed);
+    }
+  };
+
   char const* caseCmpString1 = "OnE - oF My 962946 CrAzY_CasE StRiNgs!?.";
   char const* caseCmpString2 = "oNe - Of mY 962946 cRaZy_cASe sTrInGS!?.";
 
@@ -34,183 +49,183 @@ namespace XR
     "interact",
   };
 
-    TEST(Hash, StringCaseInsensitive)
+  TEST_F(HashTests, StringCaseInsensitive)
+  {
+    auto hash1 = Hash::String(caseCmpString1);
+    auto hash2 = Hash::String(caseCmpString2);
+    ASSERT_EQ(hash1, hash2);
+  }
+
+  TEST_F(HashTests, String32CaseInsensitive)
+  {
+    auto hash1 = Hash::String32(caseCmpString1);
+    auto hash2 = Hash::String32(caseCmpString2);
+    ASSERT_EQ(hash1, hash2);
+  }
+
+  template <typename T>
+  std::wstring MakeAssertMsg(char const* s0, char const* s1, T hash)
+  {
+    std::wostringstream buf;
+    buf << s0 << " vs " << s1 << " (" << std::hex << hash << ") clash.";
+    return buf.str();
+  }
+
+  void Format(char const* name, uint32_t hash, char(&buffer)[64])
+  {
+    sprintf(buffer, "%s:\t%x", name, hash);
+  }
+
+  void Format(char const* name, uint64_t hash, char(&buffer)[64])
+  {
+    sprintf(buffer, "%s:\t%llx", name, hash);
+  }
+
+  template <typename T>
+  void DumpHashes(std::map<T, std::string> const& hashes)
+  {
+    std::map<unsigned char, int> occurences;
+
+    char arBuffer[64];
+    for (auto const& i : hashes)
     {
-      auto hash1 = Hash::String(caseCmpString1);
-      auto hash2 = Hash::String(caseCmpString2);
-      ASSERT_EQ(hash1, hash2);
-    }
-
-    TEST(Hash, String32CaseInsensitive)
-    {
-      auto hash1 = Hash::String32(caseCmpString1);
-      auto hash2 = Hash::String32(caseCmpString2);
-      ASSERT_EQ(hash1, hash2);
-    }
-
-    template <typename T>
-    std::wstring MakeAssertMsg(char const* s0, char const* s1, T hash)
-    {
-      std::wostringstream buf;
-      buf << s0 << " vs " << s1 << " (" << std::hex << hash << ") clash.";
-      return buf.str();
-    }
-
-    void Format(char const* name, uint32_t hash, char(&buffer)[64])
-    {
-      sprintf(buffer, "%s:\t%x", name, hash);
-    }
-
-    void Format(char const* name, uint64_t hash, char(&buffer)[64])
-    {
-      sprintf(buffer, "%s:\t%llx", name, hash);
-    }
-
-    template <typename T>
-    void DumpHashes(std::map<T, std::string> const& hashes)
-    {
-      std::map<unsigned char, int> occurences;
-
-      char arBuffer[64];
-      for (auto const& i : hashes)
-      {
-        ++occurences[(i.first >> (XR_BITSIZEOF(T) - 8)) & 0xff];
-        Format(i.second.c_str(), i.first, arBuffer);
-        XR_TRACE(HashTests, (arBuffer));
-      }
-
-      int max = 0;
-      unsigned char cMax = 0;
-      int min = 9999;
-      unsigned char cMin = 0;
-      for (auto const& i : occurences)
-      {
-        if (i.second > max)
-        {
-          max = i.second;
-          cMax = i.first;
-        }
-        if (i.second < min)
-        {
-          min = i.second;
-          cMin = i.first;
-        }
-        sprintf(arBuffer, "%x : %d", i.first, i.second);
-        XR_TRACE(HashTests, (arBuffer));
-      }
-
-      sprintf(arBuffer, "min: %d (%x)", min, cMin);
-      XR_TRACE(HashTests, (arBuffer));
-
-      sprintf(arBuffer, "max: %d (%x)", max, cMax);
+      ++occurences[(i.first >> (XR_BITSIZEOF(T) - 8)) & 0xff];
+      Format(i.second.c_str(), i.first, arBuffer);
       XR_TRACE(HashTests, (arBuffer));
     }
 
-    void DoStringUniqueness(bool dump)
+    int max = 0;
+    unsigned char cMax = 0;
+    int min = 9999;
+    unsigned char cMin = 0;
+    for (auto const& i : occurences)
     {
-      std::map<uint64_t, std::string> hashes;
-      char arBuffer[256];
-      for (int i = 0; i < 26; ++i)
+      if (i.second > max)
       {
-        for (int j = 0; j < 10; ++j)
-        {
-          for (int z = 0; z < XR_ARRAY_SIZE(strings); ++z)
-          {
-            auto len = sprintf(arBuffer, "ins_%c_%s%d_anims", i + 'a', strings[z], j);
-            auto hash = Hash::String(arBuffer, len, false);
-
-            auto iFind = hashes.find(hash);
-            bool hashClashing = iFind != hashes.end();
-            XR_TRACEIF(HashTests, hashClashing, ("%s", MakeAssertMsg(arBuffer, iFind->second.c_str(), hash).c_str()));
-            ASSERT_FALSE(hashClashing);
-            hashes[hash] = arBuffer;
-          }
-        }
+        max = i.second;
+        cMax = i.first;
       }
-
-      if(dump) DumpHashes(hashes);
+      if (i.second < min)
+      {
+        min = i.second;
+        cMin = i.first;
+      }
+      sprintf(arBuffer, "%x : %d", i.first, i.second);
+      XR_TRACE(HashTests, (arBuffer));
     }
 
-    TEST(Hash, StringUniqueness)
+    sprintf(arBuffer, "min: %d (%x)", min, cMin);
+    XR_TRACE(HashTests, (arBuffer));
+
+    sprintf(arBuffer, "max: %d (%x)", max, cMax);
+    XR_TRACE(HashTests, (arBuffer));
+  }
+
+  void DoStringUniqueness(bool dump)
+  {
+    std::map<uint64_t, std::string> hashes;
+    char arBuffer[256];
+    for (int i = 0; i < 26; ++i)
     {
-      Hash::SetSeed(Hash::kSeed);
+      for (int j = 0; j < 10; ++j)
+      {
+        for (int z = 0; z < XR_ARRAY_SIZE(strings); ++z)
+        {
+          auto len = sprintf(arBuffer, "ins_%c_%s%d_anims", i + 'a', strings[z], j);
+          auto hash = Hash::String(arBuffer, len, false);
+
+          auto iFind = hashes.find(hash);
+          bool hashClashing = iFind != hashes.end();
+          XR_TRACEIF(HashTests, hashClashing, ("%s", MakeAssertMsg(arBuffer, iFind->second.c_str(), hash).c_str()));
+          ASSERT_FALSE(hashClashing);
+          hashes[hash] = arBuffer;
+        }
+      }
+    }
+
+    if(dump) DumpHashes(hashes);
+  }
+
+  TEST_F(HashTests, StringUniqueness)
+  {
+    Hash::SetSeed(Hash::kSeed);
+    DoStringUniqueness(false);
+
+    for (int i = 0; i < 32; ++i)
+    {
+      Hash::SetSeed((1 << i) - 1);
       DoStringUniqueness(false);
-
-      for (int i = 0; i < 32; ++i)
-      {
-        Hash::SetSeed((1 << i) - 1);
-        DoStringUniqueness(false);
-      }
     }
+  }
 
-    void DoString32Uniqueness(bool dump)
+  void DoString32Uniqueness(bool dump)
+  {
+    std::map<uint32_t, std::string> hashes;
+    char arBuffer[256];
+    for (int i = 0; i < 26; ++i)
     {
-      std::map<uint32_t, std::string> hashes;
-      char arBuffer[256];
-      for (int i = 0; i < 26; ++i)
+      for (int j = 0; j < 10; ++j)
       {
-        for (int j = 0; j < 10; ++j)
+        for (int z = 0; z < XR_ARRAY_SIZE(strings); ++z)
         {
-          for (int z = 0; z < XR_ARRAY_SIZE(strings); ++z)
-          {
-            auto len = sprintf(arBuffer, "ins_%c_%s%d_anims", i + 'a', strings[z], j);
-            auto hash = Hash::String32(arBuffer, len);
+          auto len = sprintf(arBuffer, "ins_%c_%s%d_anims", i + 'a', strings[z], j);
+          auto hash = Hash::String32(arBuffer, len);
 
-            auto iFind = hashes.find(hash);
-            bool hashClashing = iFind != hashes.end();
-            XR_TRACEIF(HashTests, hashClashing, ("%s", MakeAssertMsg(arBuffer, iFind->second.c_str(), hash).c_str()));
-            ASSERT_FALSE(hashClashing);
-            hashes[hash] = arBuffer;
-          }
+          auto iFind = hashes.find(hash);
+          bool hashClashing = iFind != hashes.end();
+          XR_TRACEIF(HashTests, hashClashing, ("%s", MakeAssertMsg(arBuffer, iFind->second.c_str(), hash).c_str()));
+          ASSERT_FALSE(hashClashing);
+          hashes[hash] = arBuffer;
         }
       }
-
-      if (dump) DumpHashes(hashes);
     }
 
-    TEST(Hash, String32Uniqueness)
+    if (dump) DumpHashes(hashes);
+  }
+
+  TEST_F(HashTests, String32Uniqueness)
+  {
+    Hash::SetSeed(Hash::kSeed);
+    DoString32Uniqueness(false);
+
+    for (int i = 0; i <= 32; ++i)
     {
-      Hash::SetSeed(Hash::kSeed);
+      Hash::SetSeed((1 << i) - 1);
       DoString32Uniqueness(false);
-
-      for (int i = 0; i <= 32; ++i)
-      {
-        Hash::SetSeed((1 << i) - 1);
-        DoString32Uniqueness(false);
-      }
     }
+  }
 
-    //TEST(Hash, ShortString32Uniqueness)
-    //{
-    //  char const kAlphabet[] = "abcdefghijklmnopqrstuvwxyz";// 0123456789";
-    //  const auto kAlphaSize = XR_ARRAY_SIZE(kAlphabet) - 1;
-    //  char buffer[6];
-    //  char logBuffer[64];
-    //  std::map<uint32_t, std::string> hashes;
-    //  //std::set<uint32_t> hashes;
-    //  for (int k = 2; k < kAlphaSize; ++k)
-    //  {
-    //    int target = pow(kAlphaSize, k);
-    //    for (int j = 0; j < target; ++j)
-    //    {
-    //      int jj = j;
-    //      for (int i = k - 1; i >= 0; --i)
-    //      {
-    //        buffer[i] = kAlphabet[jj % kAlphaSize];
-    //        jj /= kAlphaSize;
-    //      }
+  //TEST_F(HashTests, ShortString32Uniqueness)
+  //{
+  //  char const kAlphabet[] = "abcdefghijklmnopqrstuvwxyz";// 0123456789";
+  //  const auto kAlphaSize = XR_ARRAY_SIZE(kAlphabet) - 1;
+  //  char buffer[6];
+  //  char logBuffer[64];
+  //  std::map<uint32_t, std::string> hashes;
+  //  //std::set<uint32_t> hashes;
+  //  for (int k = 2; k < kAlphaSize; ++k)
+  //  {
+  //    int target = pow(kAlphaSize, k);
+  //    for (int j = 0; j < target; ++j)
+  //    {
+  //      int jj = j;
+  //      for (int i = k - 1; i >= 0; --i)
+  //      {
+  //        buffer[i] = kAlphabet[jj % kAlphaSize];
+  //        jj /= kAlphaSize;
+  //      }
 
-    //      auto hash = Hash::String32(buffer, k);
-    //      auto iFind = hashes.find(hash);
-    //      bool assertion = iFind == hashes.end();
-    //      ASSERT_TRUE(assertion, assertion ? nullptr : MakeAssertMsg(buffer, iFind->second.c_str(), hash).c_str());
-    //      hashes.insert(iFind, { hash, std::string(buffer, k) });
-    //      //ASSERT_TRUE(assertion, assertion ? nullptr : MakeAssertMsg(buffer, buffer, hash).c_str());
-    //      //hashes.insert(hash);
-    //      //
-    //      //Format(buffer, hash, logBuffer);
-    //      //XR_TRACE(HashTests, (logBuffer));
-    //    }
-    //  }
-    //}
+  //      auto hash = Hash::String32(buffer, k);
+  //      auto iFind = hashes.find(hash);
+  //      bool assertion = iFind == hashes.end();
+  //      ASSERT_TRUE(assertion, assertion ? nullptr : MakeAssertMsg(buffer, iFind->second.c_str(), hash).c_str());
+  //      hashes.insert(iFind, { hash, std::string(buffer, k) });
+  //      //ASSERT_TRUE(assertion, assertion ? nullptr : MakeAssertMsg(buffer, buffer, hash).c_str());
+  //      //hashes.insert(hash);
+  //      //
+  //      //Format(buffer, hash, logBuffer);
+  //      //XR_TRACE(HashTests, (logBuffer));
+  //    }
+  //  }
+  //}
 }
