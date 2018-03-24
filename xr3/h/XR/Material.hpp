9475840e6c8af0cebae3a097580e3206d8ee3a0b
György Straub <gyorgy@nuclearheart.com>
2018-03-24 06:40:55 +0000

    Added documentation for Material and its format. - made OnLoaded() and OnUnloaded() protected;

--- xr3/h/XR/Material.hpp
+++ xr3/h/XR/Material.hpp
@@ -15,6 +15,17 @@ namespace XR
 {
 
 //==============================================================================
+///@brief A material asset that can be applied to geometries for rendering,
+/// creatable from a .mtl file, which is a XON document with support for the
+/// following properties:
+/// shader: the name of the file to load the shader from; required.
+/// textures: the name of the files to load the various texture stages from;
+///   optional. Currently up to 8 textures are supported which may be specified
+///   either as a XON value (for a single texture), key-value pairs for texture
+///   stage and path, i.e. 0: my/texture.png, 1: my/other_texture.png.
+/// state: the rendering states to set; a combination of the following are
+///   supported: depthTest, depthWrite, alphaBlend, cullBack, cullFront,
+///     wireframe.
 class Material: public Asset
 {
 public:
@@ -35,26 +46,38 @@ public:
   enum { kMaxTextureStages = 8 };  // TODO: expose in Gfx, remove from here.
 
   // general
+  ///@return The flags that this Material passes to XR::Gfx::SetState().
   uint32_t  GetStateFlags() const;
+
+  ///@brief Unsets @a clear, sets @a set of the state flags.
   void      OverrideStateFlags(uint32_t clear, uint32_t set);
 
+  ///@brief Sets @a texture for the given @a stage.
+  ///@note A maximum of 8 textures per Material is supported.
   void  SetTexture(int stage, Texture::Ptr const& texture);
+
+  ///@return The texture set for the given @a stage.
   Texture::Ptr  GetTexture(int stage) const;
 
+  ///@brief Sets the @a Shader used by this Material.
   void  SetShader(Shader::Ptr const& shader);
+
+  ///@return The Shader used by this Material.
   Shader::Ptr GetShader();
 
+  ///@brief Sets the Material for the subsequent drawing commands,
+  /// passing its state, texture and shader information to Gfx.
   void Apply() const;
 
-  // Inherited via Asset
-  bool OnLoaded(Buffer buffer) override;
-
-  void OnUnload() override;
-
 private:
+  // data
   uint32_t      m_stateFlags = 0;
   Texture::Ptr  m_textureStages[kMaxTextureStages];
   Shader::Ptr   m_shader;
+
+  // internal
+  bool OnLoaded(Buffer buffer) override;
+  void OnUnload() override;
 };
 
 } // XR
