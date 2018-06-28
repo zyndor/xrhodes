277024a507488057b6972025f10d0e030f287871
György Straub <gyorgy@nuclearheart.com>
2018-06-28 17:21:40 +0100

    Added xr::Rect::IsZero(), Intersection() and Union().

--- xr3/h/xr/Rect.hpp
+++ xr3/h/xr/Rect.hpp
@@ -7,7 +7,7 @@
 // copyright (c) Nuclear Heart Interactive Ltd. All rights reserved.
 //
 //==============================================================================
-
+#include <algorithm>
 #include <cstdint>
 
 namespace xr
@@ -28,6 +28,31 @@ public:
   Rect(int32_t x_, int32_t y_, int32_t w_, int32_t h_)
   : x(x_), y(y_), w(w_), h(h_)
   {}
+
+  // general
+  ///@brief Whether any of the dimensions of this is zero.
+  bool IsZero() const
+  {
+    return w == 0 || h == 0;
+  }
+
+  ///@return A rectangle that the common area between this and @a rhs encloses.
+  Rect Intersection(Rect const& rhs) const
+  {
+    Rect result(std::max(x, rhs.x), std::max(y, rhs.y), 0, 0);
+    result.w = std::max(std::min(x + w, rhs.x + rhs.w) - result.x, 0);
+    result.h = std::max(std::min(y + h, rhs.y + rhs.h) - result.y, 0);
+    return result;
+  }
+
+  ///@return A rectangle that contains both this and @a rhs.
+  Rect  Union(Rect const& rhs) const
+  {
+    Rect result(std::min(x, rhs.x), std::min(y, rhs.y), 0, 0);
+    result.w = std::max(x + w, rhs.x + rhs.w) - result.x;
+    result.h = std::max(y + h, rhs.y + rhs.h) - result.y;
+    return result;
+  }
 };
 
 } // XR
