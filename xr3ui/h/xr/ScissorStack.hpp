c828d99c21f3844670c59cf48872f81307ba30ca
György Straub <gyorgy@nuclearheart.com>
2018-06-28 17:20:27 +0100

    Added xr::ScissorStack.
    - changed UIHorizontal- and -VerticalScrollingLayout() to use ScissorStack;

---
+++ xr3ui/h/xr/ScissorStack.hpp
@@ -0,0 +1,56 @@
+#ifndef XR_SCISSORSTACK_HPP
+#define XR_SCISSORSTACK_HPP
+//==============================================================================
+//
+// XRhodes
+//
+// copyright (c) Nuclear Heart Interactive Ltd. All rights reserved.
+//
+//==============================================================================
+#include "xr/Rect.hpp"
+#include "xr/fundamentals.hpp"
+
+namespace xr
+{
+
+//==============================================================================
+///@brief Provides convenience for storing a history of rectangles used for
+/// scissor testing, and switching back to previous ones without requiring
+/// client code to have access to them.
+class ScissorStack
+{
+  XR_NONOBJECT_DECL(ScissorStack)
+
+public:
+  ///@brief ScissorStack Push-Pop frame that cleans itself up.
+  struct Scope
+  {
+    Scope(Rect const* rect, bool intersect);
+    ~Scope();
+  };
+
+  // static
+  ///@brief Initializes the ScissorStack.
+  static void Init();
+
+  ///@brief Initializes the ScissorStack only if it has not been yet.
+  static void RequireInit();
+
+  ///@brief Sets the scissor rectangle and stores it on top of the stack.
+  ///@note No ownership transfer. The rectangles are assumed to outlive the
+  /// call to Pop() that is supposed to remove them.
+  static void Push(Rect const* rect, bool intersect);
+
+  ///@return The current scissor rect known to the stack, including intersections
+  /// where requested.
+  static Rect const* GetCurrent();
+
+  ///@brief Removes the rectangle on the top of the stack (there must be one),
+  /// and sets the one on the new top of the stack (or disables scissor
+  /// testing if the stack is now empty).
+  static void Pop();
+};
+
+};
+
+#endif //XR_SCISSORSTACK_HPP
