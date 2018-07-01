7d58c813e3bc5a5e1f15e78d1fe733936c48acb0
György Straub <gyorgy@nuclearheart.com>
2018-07-01 05:58:49 +0100

    Added RectTests.

---
+++ unittests/src/RectTests.cpp
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+#include "xr/Rect.hpp"
+
+namespace xr
+{
+
+void AssertEqual(Rect const& lhs, Rect const& rhs)
+{
+  ASSERT_EQ(lhs.x, rhs.x);
+  ASSERT_EQ(lhs.y, rhs.y);
+  ASSERT_EQ(lhs.w, rhs.w);
+  ASSERT_EQ(lhs.h, rhs.h);
+}
+
+TEST(Rect, BasicOperations)
+{
+  Rect r0(-50, -50, 100, 100);
+  Rect r1(0, 0, 100, 20);
+
+  ASSERT_TRUE(!r0.IsZero());
+
+  Rect u = r0.Union(r1);
+  AssertEqual(u, Rect(-50, -50, 150, 100));
+  ASSERT_FALSE(u.IsZero());
+
+  Rect i = r0.Intersection(r1);
+  AssertEqual(i, Rect(0, 0, 50, 20));
+  ASSERT_FALSE(i.IsZero());
+
+  r0 = Rect(100, 100, 20, 20);
+  i = r0.Intersection(r1);
+  ASSERT_TRUE(i.IsZero());
+}
+
+}
