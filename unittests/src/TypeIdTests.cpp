f7cd38741bf127f489e333269a2daa0d69bf4f8a
György Straub <gyorgy@nuclearheart.com>
2019-03-30 05:41:09 +0000

    Make TypeId<>() work on (MSVC) release builds as well.
    - Added TypeIdTests;

---
+++ unittests/src/TypeIdTests.cpp
@@ -0,0 +1,41 @@
+//
+// XRhodes
+//
+// copyright (c) Gyorgy Straub. All rights reserved.
+//
+// License: https://github.com/zyndor/xrhodes#License-bsd-2-clause
+//
+//==============================================================================
+#include "gtest/gtest.h"
+#include "xr/types/typeutils.hpp"
+
+using namespace xr;
+
+struct FooBase
+{
+  virtual ~FooBase() {}
+};
+
+struct Foo : public FooBase
+{};
+
+using MyFoo = Foo;
+
+template <typename T>
+struct FooT
+{
+  T* data;
+};
+
+TEST(TypeId, Basics)
+{
+  ASSERT_TRUE(TypeId<FooBase>() == TypeId<FooBase>());
+  ASSERT_TRUE(TypeId<Foo>() != TypeId<FooBase>());
+  ASSERT_TRUE(TypeId<MyFoo>() == TypeId<Foo>());
+
+  ASSERT_TRUE(TypeId<FooT<int>>() != TypeId<FooT<char>>());
+  ASSERT_TRUE(TypeId<Foo>() != TypeId<FooT<int>>());
+  ASSERT_TRUE(TypeId<FooBase>() != TypeId<FooT<int>>());
+  ASSERT_TRUE(TypeId<Foo>() != TypeId<FooT<char>>());
+  ASSERT_TRUE(TypeId<FooBase>() != TypeId<FooT<char>>());
+}
