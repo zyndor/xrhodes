5e312512c713cf3106cfb0633b462899ce96fbdb
György Straub <gyorgy@nuclearheart.com>
2019-10-23 08:03:30 +0100

    Added xr::ScratchBuffer::Handles and Bake() to allow getting vertex / index buffer objects out of it .
    - rewrote the innards of ScratchBuffer::RenderBuffers() based on Bake();
    - Bake() flushes the pool internally;
    - restricted AllocateIndices() to be callable after Start{...}();
    - deprecated Flush() (which is now no-op) and raw Allocate() (which returns nullptr);
    - removed the registration for Gfx::FlushSignal;
    - added code comments and cleaned up naming;

--- xr3/src/ScratchBuffer.cpp
+++ xr3/src/ScratchBuffer.cpp
@@ -20,171 +20,173 @@ class ScratchBufferImpl
 public:
   ScratchBufferImpl(size_t poolSize)
   {
-    m_pool.SetBuffer(poolSize, true, nullptr);
-  }
-
-  void* Allocate(size_t numBytes)
-  {
-    return m_pool.Allocate(numBytes);
+    mPool.SetBuffer(poolSize, true, nullptr);
   }
 
   template <class VertexFormat>
   VertexFormat* AllocateVertices(uint32_t numVertices)
   {
-    XR_ASSERTMSG(ScratchBuffer, !m_hFormat.IsValid(), ("Call Finish() before starting a new scratch!"));
+    XR_ASSERTMSG(ScratchBuffer, !mFormat.IsValid(), ("Call Finish() before starting a new scratch!"));
     size_t size = numVertices * VertexFormat::kSize;
-    auto buffer = m_pool.Allocate(size);
+    auto buffer = mPool.Allocate(size);
     if (buffer)
     {
-      m_hFormat = Vertex::Formats::GetHandle<VertexFormat>();
-      m_numVertices = numVertices;
-      m_vertices = { size, static_cast<uint8_t*>(buffer) };
+      mFormat = Vertex::Formats::GetHandle<VertexFormat>();
+      mNumVertices = numVertices;
+      mVertices = { size, static_cast<uint8_t*>(buffer) };
     }
     return static_cast<VertexFormat*>(buffer);
   }
 
   uint16_t* AllocateIndices(uint32_t numIndices)
   {
-    XR_ASSERTMSG(ScratchBuffer, !m_indices.data, ("Call Finish() before starting a new scratch!"));
+    XR_ASSERTMSG(ScratchBuffer, mVertices.data, ("Call Start{...}() before allocating indices!"));
+    XR_ASSERTMSG(ScratchBuffer, !mIndices.data, ("Call Finish() before starting a new scratch!"));
     size_t size = numIndices * sizeof(uint16_t);
-    auto buffer = m_pool.Allocate(size);
+    auto buffer = mPool.Allocate(size);
     if (buffer)
     {
-      m_numIndices = numIndices;
-      m_indices = { size, static_cast<uint8_t*>(buffer) };
+      mNumIndices = numIndices;
+      mIndices = { size, static_cast<uint8_t*>(buffer) };
     }
     return static_cast<uint16_t*>(buffer);
   }
 
+  ScratchBuffer::Handles Bake()
+  {
+    XR_ASSERTMSG(ScratchBuffer, mVertices.data, ("No vertices allocated for drawing."));
+    ScratchBuffer::Handles result = { Gfx::CreateVertexBuffer(mFormat, mVertices) };
+    mVertices = { 0, nullptr };
+    mFormat.Invalidate();
+    if (mIndices.data)
+    {
+      result.mIbo = Gfx::CreateIndexBuffer(mIndices);
+      mIndices = { 0, nullptr };
+    }
+    mPool.Flush();
+    return result;
+  }
+
   void RenderBuffers(Primitive primitive)
   {
-    XR_ASSERTMSG(ScratchBuffer, m_vertices.data, ("No vertices allocated for drawing."));
-    auto vbo = Gfx::CreateVertexBuffer(m_hFormat, m_vertices);
-    if (m_indices.data)
+    XR_ASSERTMSG(ScratchBuffer, mVertices.data, ("No vertices allocated for drawing."));
+    auto handles = Bake();
+    if (handles.mIbo.IsValid())
     {
-      auto ibo = Gfx::CreateIndexBuffer(m_indices);
-      Gfx::Draw(vbo, ibo, primitive, 0, m_numIndices);
+      Gfx::Draw(handles.mVbo, handles.mIbo, primitive, 0, mNumIndices);
 
-      Gfx::Release(ibo);
+      Gfx::Release(handles.mIbo);
     }
     else
     {
-      Gfx::Draw(vbo, primitive, 0, m_numVertices);
+      Gfx::Draw(handles.mVbo, primitive, 0, mNumVertices);
     }
 
-    Gfx::Release(vbo);
-
-    m_vertices = { 0, nullptr };
-    m_indices = { 0, nullptr };
-    m_hFormat.Invalidate();
-  }
-
-  void Flush()
-  {
-    m_pool.Flush();
+    Gfx::Release(handles.mVbo);
   }
 
 private:
-  Pool m_pool;
+  Pool mPool;
 
-  Gfx::VertexFormatHandle m_hFormat;  // no ownership
-  uint32_t m_numVertices = 0;
-  Buffer m_vertices = { 0, nullptr };
+  Gfx::VertexFormatHandle mFormat;  // no ownership
+  uint32_t mNumVertices = 0;
+  Buffer mVertices = { 0, nullptr };
 
-  uint32_t m_numIndices = 0;
-  Buffer m_indices = { 0, nullptr };
+  uint32_t mNumIndices = 0;
+  Buffer mIndices = { 0, nullptr };
 };
 
-ScratchBufferImpl*  s_impl = nullptr;
+ScratchBufferImpl*  sImpl = nullptr;
 
-} //
+} // nonamespace
 
 //==============================================================================
 void ScratchBuffer::Init(size_t poolSize)
 {
-  XR_ASSERTMSG(ScratchBuffer, !s_impl, ("Already initialized!"));
-  s_impl = new ScratchBufferImpl(poolSize);
-
-  Gfx::FlushSignal().Connect(FunctionPtrCallback<void>([](void*) {
-    Flush();
-  }, nullptr));
+  XR_ASSERTMSG(ScratchBuffer, !sImpl, ("Already initialized!"));
+  sImpl = new ScratchBufferImpl(poolSize);
 
   Gfx::ShutdownSignal().Connect(FunctionPtrCallback<void>([](void*) {
-    delete s_impl;
-    s_impl = nullptr;
+    delete sImpl;
+    sImpl = nullptr;
   }, nullptr));
 }
 
 //==============================================================================
 void* ScratchBuffer::Allocate(size_t numBytes)
 {
-  return s_impl->Allocate(numBytes);
+  return nullptr;
 }
 
 //==============================================================================
 ScratchBuffer::Position2d* ScratchBuffer::Start2d(uint32_t numVertices)
 {
-  return s_impl->AllocateVertices<Position2d>(numVertices);
+  return sImpl->AllocateVertices<Position2d>(numVertices);
 }
 
 //==============================================================================
-ScratchBuffer::Position3d * ScratchBuffer::Start3d(uint32_t numVertices)
+ScratchBuffer::Position3d* ScratchBuffer::Start3d(uint32_t numVertices)
 {
-  return s_impl->AllocateVertices<Position3d>(numVertices);
+  return sImpl->AllocateVertices<Position3d>(numVertices);
 }
 
 //==============================================================================
-ScratchBuffer::Position2dUv * ScratchBuffer::Start2dUv(uint32_t numVertices)
+ScratchBuffer::Position2dUv* ScratchBuffer::Start2dUv(uint32_t numVertices)
 {
-  return s_impl->AllocateVertices<Position2dUv>(numVertices);
+  return sImpl->AllocateVertices<Position2dUv>(numVertices);
 }
 
 //==============================================================================
-ScratchBuffer::Position3dUv * ScratchBuffer::Start3dUv(uint32_t numVertices)
+ScratchBuffer::Position3dUv* ScratchBuffer::Start3dUv(uint32_t numVertices)
 {
-  return s_impl->AllocateVertices<Position3dUv>(numVertices);
+  return sImpl->AllocateVertices<Position3dUv>(numVertices);
 }
 
 //==============================================================================
-ScratchBuffer::Position2dUvColor * ScratchBuffer::Start2dUvColor(uint32_t numVertices)
+ScratchBuffer::Position2dUvColor* ScratchBuffer::Start2dUvColor(uint32_t numVertices)
 {
-  return s_impl->AllocateVertices<Position2dUvColor>(numVertices);
+  return sImpl->AllocateVertices<Position2dUvColor>(numVertices);
 }
 
 //==============================================================================
-ScratchBuffer::Position3dUvColor * ScratchBuffer::Start3dUvColor(uint32_t numVertices)
+ScratchBuffer::Position3dUvColor* ScratchBuffer::Start3dUvColor(uint32_t numVertices)
 {
-  return s_impl->AllocateVertices<Position3dUvColor>(numVertices);
+  return sImpl->AllocateVertices<Position3dUvColor>(numVertices);
 }
 
 //==============================================================================
-ScratchBuffer::Position2dColor * ScratchBuffer::Start2dColor(uint32_t numVertices)
+ScratchBuffer::Position2dColor* ScratchBuffer::Start2dColor(uint32_t numVertices)
 {
-  return s_impl->AllocateVertices<Position2dColor>(numVertices);
+  return sImpl->AllocateVertices<Position2dColor>(numVertices);
 }
 
 //==============================================================================
-ScratchBuffer::Position3dColor * ScratchBuffer::Start3dColor(uint32_t numVertices)
+ScratchBuffer::Position3dColor* ScratchBuffer::Start3dColor(uint32_t numVertices)
 {
-  return s_impl->AllocateVertices<Position3dColor>(numVertices);
+  return sImpl->AllocateVertices<Position3dColor>(numVertices);
 }
 
 //==============================================================================
-uint16_t * ScratchBuffer::AllocateIndices(uint32_t numIndices)
+uint16_t* ScratchBuffer::AllocateIndices(uint32_t numIndices)
 {
-  return s_impl->AllocateIndices(numIndices);
+  return sImpl->AllocateIndices(numIndices);
+}
+
+//==============================================================================
+ScratchBuffer::Handles ScratchBuffer::Bake()
+{
+  return sImpl->Bake();
 }
 
 //==============================================================================
 void ScratchBuffer::Finish(Primitive primitive)
 {
-  s_impl->RenderBuffers(primitive);
+  sImpl->RenderBuffers(primitive);
 }
 
 //==============================================================================
 void ScratchBuffer::Flush()
 {
-  s_impl->Flush();
 }
 
 } // XR
