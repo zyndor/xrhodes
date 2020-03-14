bc3c43f1f4181511ae5c4f8bef06ac7d65882ab5
György Straub <gyorgy@nuclearheart.com>
2020-03-14 10:00:18 +0000

    Added xr::Stick, an immutable string type constructable from string literals, storing string length and providing range based for support.

---
+++ xr3core/h/xr/strings/Stick.hpp
@@ -0,0 +1,70 @@
+#ifndef XR_STICK_HPP
+#define XR_STICK_HPP
+//==============================================================================
+//
+// XRhodes
+//
+// copyright (c) Gyorgy Straub. All rights reserved.
+//
+// License: https://github.com/zyndor/xrhodes#License-bsd-2-clause
+//
+//==============================================================================
+#include <cstdint>
+
+namespace xr
+{
+
+//==============================================================================
+///@brief Immutable string type which stores the size of the string, and offers
+/// range based for support which ignores terminating null.
+struct Stick
+{
+  // types
+  using SizeType = uint32_t;
+
+  // data
+  const char* const mData;
+  const SizeType mSize;
+
+  // structors
+  template <SizeType kLength>
+  constexpr Stick(char const(&string)[kLength]);
+
+  // range based for support
+  const char* const begin() const;
+  const char* const end() const;
+
+  // operator overloads
+    operator const char*() const;
+};
+
+//==============================================================================
+// implementation
+//==============================================================================
+template <Stick::SizeType kLength>
+constexpr Stick::Stick(char const(&string)[kLength])
+: mData(string),
+  mSize(kLength - 1)
+{}
+
+//==============================================================================
+const char* const Stick::begin() const
+{
+  return mData;
+}
+
+//==============================================================================
+const char* const Stick::end() const
+{
+  return mData + mSize;
+}
+
+//==============================================================================
+Stick::operator const char*() const
+{
+  return mData;
+}
+
+}
+
+#endif  //XR_STICK_HPP
