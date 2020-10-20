535b666e1205ba49598f16a7e3b823cdb8df0c62
György Straub <gyorgy@nuclearheart.com>
2020-10-20 20:05:56 +0100

    Fix: attempting to create an empty vertex / index / instance data buffer no longer results in an invalid handle.

--- xr3/src/GfxM.cpp
+++ xr3/src/GfxM.cpp
@@ -35,6 +35,7 @@ uint8_t* CopyBuffer(Buffer const& buffer)
   if (buffer.size > 0)
   {
     bufferCopy = Alloc<uint8_t>(buffer.size);
+    XR_ASSERTMSG(Gfx, bufferCopy, ("Failed to allocate %" PRIu32 " bytes", buffer.size));
     std::memcpy(bufferCopy, buffer.data, buffer.size);
   }
   return bufferCopy;
@@ -826,18 +827,15 @@ VertexBufferHandle M::CreateVertexBuffer(VertexFormatHandle hFormat,
 {
   VertexBufferHandle h;
   auto bufferCopy = CopyBuffer(buffer);
-  if (bufferCopy)
+  auto& vbos = sContext->GetResources().GetVbos();
   {
-    auto& vbos = sContext->GetResources().GetVbos();
-    {
-      std::unique_lock<Spinlock> lock(sContext->GetResources().GetLock());
-      h.id = static_cast<uint16_t>(vbos.server.Acquire());
-    };
+    std::unique_lock<Spinlock> lock(sContext->GetResources().GetLock());
+    h.id = static_cast<uint16_t>(vbos.server.Acquire());
+  };
 
-    sContext->GetActiveQueue().WriteCommand(Command::CreateVertexBuffer,
-      CreateVertexBufferMessage{ hFormat, Buffer{ buffer.size, bufferCopy },
-        flags, vbos.data + h.id });
-  }
+  sContext->GetActiveQueue().WriteCommand(Command::CreateVertexBuffer,
+    CreateVertexBufferMessage{ hFormat, Buffer{ buffer.size, bufferCopy },
+      flags, vbos.data + h.id });
   return h;
 }
 
@@ -852,18 +850,15 @@ IndexBufferHandle M::CreateIndexBuffer(Buffer const& buffer, FlagType flags)
 {
   IndexBufferHandle h;
   auto bufferCopy = CopyBuffer(buffer);
-  if (bufferCopy)
+  auto& ibos = sContext->GetResources().GetIbos();
   {
-    auto& ibos = sContext->GetResources().GetIbos();
-    {
-      std::unique_lock<Spinlock> lock(sContext->GetResources().GetLock());
-      h.id = static_cast<uint16_t>(ibos.server.Acquire());
-    };
+    std::unique_lock<Spinlock> lock(sContext->GetResources().GetLock());
+    h.id = static_cast<uint16_t>(ibos.server.Acquire());
+  };
 
-    sContext->GetActiveQueue().WriteCommand(Command::CreateIndexBuffer,
-      CreateIndexBufferMessage{ Buffer { buffer.size, bufferCopy }, flags,
-        ibos.data + h.id });
-  }
+  sContext->GetActiveQueue().WriteCommand(Command::CreateIndexBuffer,
+    CreateIndexBufferMessage{ Buffer { buffer.size, bufferCopy }, flags,
+      ibos.data + h.id });
   return h;
 }
 
@@ -878,18 +873,15 @@ InstanceDataBufferHandle M::CreateInstanceDataBuffer(Buffer const& buffer, uint1
 {
   InstanceDataBufferHandle h;
   auto bufferCopy = CopyBuffer(buffer);
-  if (bufferCopy)
+  auto& vbos = sContext->GetResources().GetVbos();
   {
-    auto& vbos = sContext->GetResources().GetVbos();
-    {
-      std::unique_lock<Spinlock> lock(sContext->GetResources().GetLock());
-      h.id = static_cast<uint16_t>(vbos.server.Acquire());
-    }
-
-    sContext->GetActiveQueue().WriteCommand(Command::CreateInstanceDataBuffer,
-      CreateInstanceDataBufferMessage{ Buffer { buffer.size, bufferCopy }, stride,
-        vbos.data + h.id });
+    std::unique_lock<Spinlock> lock(sContext->GetResources().GetLock());
+    h.id = static_cast<uint16_t>(vbos.server.Acquire());
   }
+
+  sContext->GetActiveQueue().WriteCommand(Command::CreateInstanceDataBuffer,
+    CreateInstanceDataBufferMessage{ Buffer { buffer.size, bufferCopy }, stride,
+      vbos.data + h.id });
   return h;
 }
 
