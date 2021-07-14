6efca23c97035e3d2b116a48051ec0985510214e
György Straub <gyorgy@nuclearheart.com>
2021-07-14 21:03:27 +0100

    Suppressed GCC warnings in stb_truetype.cpp.

--- xr3/src/stb_truetype.cpp
+++ xr3/src/stb_truetype.cpp
@@ -7,5 +7,13 @@
 // License: https://github.com/zyndor/xrhodes#License-bsd-2-clause
 //
 //==============================================================================
+#include "xr/platform.hpp"
+
+#if defined XR_COMPILER_GCC
+#pragma GCC diagnostic ignored "-Wdouble-promotion"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#endif
+
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
