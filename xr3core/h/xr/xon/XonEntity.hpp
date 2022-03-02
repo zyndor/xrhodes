ecb0675afb530c3675bba77594d805442c409c34
György Straub <gyorgy@nuclearheart.com>
2022-03-02 21:03:46 +0000

    XonEntity modernisation / improvements.
    - XonObject and XonValue are both final classes;
    - Removed  the m_type data member in favour of the now virtual GetType() providing this information;
    - XonObject holds std::unique_ptrs;
    - added XonObject::GetKeys() overloads taking a functor and output iterator;
    - deprecated XonObject::AddElement() in favour of an overload that takes the key as a std::string_view;
    - updated XonBuildTree;

--- xr3core/h/xr/xon/XonEntity.hpp
+++ xr3core/h/xr/xon/XonEntity.hpp
@@ -12,6 +12,9 @@
 #include "xr/types/fundamentals.hpp"
 #include <map>
 #include <vector>
+#include <memory>
+#include <string>
+#include <string_view>
 
 namespace xr
 {
@@ -57,10 +60,8 @@ public:
   virtual ~XonEntity();
 
   // general
-  Type GetType() const
-  {
-    return m_type;
-  }
+  ///@return The type of the entity.
+  virtual Type GetType() const = 0;
 
   ///@return Whether this is the given @a type.
   bool Is(Type type) const;
@@ -83,28 +84,26 @@ public:
 
 protected:
   // structors
-  explicit XonEntity(Type t);
-
-private:
-  // data
-  Type m_type;
+  XonEntity();
 };
 
 //==============================================================================
 ///@brief XON object - a container for other XON entities. Does not have a value
 /// of its own. The elements may be indexed in the order of their declaration.
 /// Additionally, elements declared with a key may be accessed using the key.
-class XonObject : public XonEntity
+class XonObject final : public XonEntity
 {
 public:
   // types
-  using Xontainer = std::vector<XonEntity*>;
+  using Xontainer = std::vector<std::unique_ptr<XonEntity>>;
 
   // structors
   XonObject();
   ~XonObject();
 
   // general
+  Type GetType() const override;
+
   ///@return A pointer to the element mapped to the given @a key; nullptr if
   /// there is no such element.
   ///@note Does not transfer ownership.
@@ -128,6 +127,28 @@ public:
     return m_elements.size();
   }
 
+  ///@brief Passes each of its keys to @a fn.
+  template <typename Fn,
+    std::enable_if_t<std::is_member_function_pointer_v<decltype(&Fn::operator())>>* = nullptr>
+  void GetKeys(Fn fn) const
+  {
+    for (auto& kv : m_keyedElements)
+    {
+      fn(kv.first);
+    }
+  }
+
+  ///@brief Assigns each of its keys to output iterator @a i.
+  template <typename OutIt,
+    std::enable_if_t<std::is_member_function_pointer_v<decltype(&OutIt::operator*)>>* = nullptr>
+  void GetKeys(OutIt i) const
+  {
+    for (auto& kv : m_keyedElements)
+    {
+      *i = kv.first;
+    }
+  }
+
   ///@brief Writes the keys of its elements into @a keys, in lexicographic order.
   void GetKeys(std::vector<std::string>& keys) const;
 
@@ -137,7 +158,10 @@ public:
   ///@brief Adds the given element @a elem as a child at the next index, also
   /// keyed to @a key.
   ///@note Overrides a previous mapping of the same key (if any).
-  void AddElement(std::string key, XonEntity& elem);  // ownership transfer
+  void AddElement(std::string_view key, XonEntity& elem);  // ownership transfer
+
+  [[deprecated]]
+  void AddElement(std::string key, XonEntity& elem) { AddElement(std::string_view{ key }, elem); }
 
   ///@return Whether this has the given element @a elem.
   bool HasElement(XonEntity const& elem) const;
@@ -150,12 +174,12 @@ public:
   Xontainer::const_iterator end() const { return m_elements.cend(); }
 
   // operators
-  ///@return a referenec to the @a index th element this object has. A XonException
+  ///@return a reference to the @a index th element this object has. A XonException
   /// of type IndexOutOfBounds will be thrown if index is outside of the range
   /// of valid indices.
   XonEntity&  operator[](size_t index);
 
-  ///@return a const referenec to the @a index th element this object has. A
+  ///@return a const reference to the @a index th element this object has. A
   /// XonException of type IndexOutOfBounds will be thrown if index is outside
   /// of the range of valid indices.
   XonEntity const&  operator[](size_t index) const;
@@ -163,12 +187,12 @@ public:
 private:
   // data
   Xontainer m_elements; // ownership
-  std::map<std::string, XonEntity*> m_keyedElements;  // no ownership
+  std::map<std::string, XonEntity*, std::less<>> m_keyedElements;  // no ownership
 };
 
 //==============================================================================
 ///@brief A XON value, stored as a string.
-class XonValue : public XonEntity
+class XonValue final : public XonEntity
 {
 public:
   // structors
@@ -178,6 +202,8 @@ public:
   ~XonValue();
 
   // general
+  Type GetType() const override;
+
   ///@brief returns the value of this element.
   const char* GetString() const
   {
